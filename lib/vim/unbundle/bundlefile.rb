983b6a998451903bd61d2fd1105a9dec647c8697
Yoshihiro Hara <haraplusplus@gmail.com>
2013-04-26 13:15:37 +0900

    Add methods for bundle definition

--- lib/vim/unbundle/bundlefile.rb
+++ lib/vim/unbundle/bundlefile.rb
@@ -5,7 +5,38 @@ module Vim
   module Unbundle
 
     class Bundlefile
+      # Get defined bundles.
+      attr_reader   :bundles
 
+      # Initializes a new Bundlefile.
+      def initialize
+        @bundles = []
+        @filetypes = [nil]
+      end
+
+      # Defines a bundle.
+      def bundle(name)
+        bundle = Bundle.new(name: name,
+                            filetype: current_filetype)
+        self.bundles << bundle
+      end
+
+      # Defines a filetype scope.
+      def filetype(ft, &block)
+        begin
+          @filetypes.push ft
+          instance_eval &block
+        ensure
+          @filetypes.pop
+        end
+      end
+
+      private
+
+      # Get the current filetype scope.
+      def current_filetype
+        @filetypes.last
+      end
 
     end
 
--- spec/vim/unbundle/bundlefile_spec.rb
+++ spec/vim/unbundle/bundlefile_spec.rb
@@ -5,6 +5,28 @@ include Vim::Unbundle
 
 describe Bundlefile do
 
-  
+  describe '#bundle' do
+    subject(:bundlefile) { Bundlefile.new }
+
+    context 'with a name' do
+      it 'defines a new bundle' do
+        bundlefile.bundle 'foo/bar'
+        expect(bundlefile.bundles.first.name).to eq('foo/bar')
+      end
+    end
+
+  end
+
+  describe '#filetype' do
+    subject(:bundlefile) { Bundlefile.new }
+
+    context 'with a filetype' do
+      it 'defines a ftbundle' do
+        bundlefile.filetype(:ruby) { bundle 'foo/bar' }
+        expect(bundlefile.bundles.first.filetype).to eq(:ruby)
+      end
+    end
+
+  end
 
 end
