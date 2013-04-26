1d8352d1eed171a9d0f6423430eba2847679c8d8
Yoshihiro Hara <haraplusplus@gmail.com>
2013-04-26 12:19:02 +0900

    Removes the template spec

--- spec/vim/unbundle_spec.rb
+++ spec/vim/unbundle_spec.rb
@@ -4,8 +4,4 @@ describe Vim::Unbundle do
   it 'should have a version number' do
     Vim::Unbundle::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
