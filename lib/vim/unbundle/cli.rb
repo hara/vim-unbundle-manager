066b1458b5367f255a034e0a9c787c2b0e43fd90
Yoshihiro Hara <haraplusplus@gmail.com>
2013-04-28 11:55:22 +0900

    Adds update interface

--- lib/vim/unbundle/cli.rb
+++ lib/vim/unbundle/cli.rb
@@ -10,20 +10,66 @@ module Vim
 
       desc 'install', 'Install bundles'
       def install
-        bundlefile = Bundlefile.load('Bundlefile')
-        raise Thor::Error, 'Cannot find Bundlefile' if bundlefile.nil?
+        bundlefile = load_bundlefile
 
         bundlefile.bundles.each do |bundle|
           if bundle.installed?
-            say "Using #{bundle.short_name}" +
-              (bundle.revision.nil? ? '' : " (#{bundle.revision})")
+            say using_message(bundle)
             next
           end
 
-          say "Installing #{bundle.short_name}'" +
-              (bundle.revision.nil? ? '' : " (#{bundle.revision})")
+          say installing_message(bundle)
           bundle.install
         end
+      end
+
+      desc 'update', 'Update bundles'
+      def update
+        bundlefile = load_bundlefile
+
+        bundlefile.bundles.each do |bundle|
+          unless bundle.installed?
+            say installing_message(bundle)
+            bundle.install
+            next
+          end
+
+          unless bundle.working_directory?
+            say using_message(bundle)
+            next
+          end
+
+          say updating_message(bundle)
+          bundle.update
+        end
+
+      end
+
+      private
+
+      # Loads the 'Bundlefile'.
+      def load_bundlefile
+        file = Bundlefile.load('Bundlefile')
+        raise Thor::Error, 'Cannot find Bundlefile' if file.nil?
+        file
+      end
+
+      # Gets the String 'Using bundle (rev)' message.
+      def using_message(bundle)
+        "Using #{bundle.short_name}" +
+          (bundle.revision.nil? ? '' : " (#{bundle.revision})")
+      end
+
+      # Gets the String 'Installing bundle (rev)' message.
+      def installing_message(bundle)
+        "Installing #{bundle.short_name}'" +
+          (bundle.revision.nil? ? '' : " (#{bundle.revision})")
+      end
+
+      # Gets the String 'Updating bundle (rev)' message.
+      def updating_message(bundle)
+        "Updating #{bundle.short_name}'" +
+          (bundle.revision.nil? ? '' : " (#{bundle.revision})")
       end
 
     end
